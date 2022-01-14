52a209ba1248f2d522b1fa0ce3f71d7b3e0c3e4d
hyc <huangyc@hshbao.com>
2022-01-14 18:00:29 +0800

    stash

--- lib/views/category.dart
+++ lib/views/category.dart
@@ -11,43 +11,63 @@ class CategoryList extends StatefulWidget {
 class _CategoryListState extends State<CategoryList> {
   Dio dio = Dio();
 
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Components"),
-        ),
-        body: Column(children: [
-          const ListTile(
-              title: Text("Demo", style: TextStyle(color: Colors.grey))),
-          FutureBuilder(
-              future: dio.get(
-                  "https://www.fastmock.site/mock/b3f7f593e7e6f1945c9836fbabd698af/api/api/category/list"),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                //请求完成
-                if (snapshot.connectionState == ConnectionState.done) {
-                  Response response = snapshot.data;
-                  //发生错误
-                  if (snapshot.hasError) {
-                    return Text(snapshot.error.toString());
-                  }
-
-                  return Expanded(
-                      child: ListView(
-                    children: response.data
-                        .map<Widget>((e) => ListTile(
-                              title: Text(e["name"]),
-                              trailing: const Icon(Icons.keyboard_arrow_right),
-                              onTap: () {
-                                Navigator.pushNamed(context, e["path"]);
-                              },
-                            ))
-                        .toList(),
-                  ));
+      appBar: AppBar(
+        title: const Text("Components"),
+      ),
+      body: Column(children: [
+        const ListTile(
+            title: Text("Demo", style: TextStyle(color: Colors.grey))),
+        FutureBuilder(
+            future: dio.get(
+                "https://www.fastmock.site/mock/b3f7f593e7e6f1945c9836fbabd698af/api/api/category/list"),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              //请求完成
+              if (snapshot.connectionState == ConnectionState.done) {
+                Response response = snapshot.data;
+                //发生错误
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
                 }
-                //请求未完成时弹出loading
-                return const CircularProgressIndicator();
-              }),
-        ]));
+
+                return Expanded(
+                    child: ListView(
+                  children: response.data
+                      .map<Widget>((e) => ListTile(
+                            title: Text(e["name"]),
+                            trailing: const Icon(Icons.keyboard_arrow_right),
+                            onTap: () {
+                              Navigator.pushNamed(context, e["path"]);
+                            },
+                          ))
+                      .toList(),
+                ));
+              }
+              //请求未完成时弹出loading
+              return const CircularProgressIndicator();
+            }),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: 'Category'),
+          BottomNavigationBarItem(icon: Icon(Icons.games), label: 'Kana'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'My'),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
--- lib/views/components/box.dart
+++ lib/views/components/box.dart
@@ -8,13 +8,41 @@ class BoxDemo extends StatefulWidget {
 }
 
 class _BoxDemoState extends State<BoxDemo> {
+  Widget redBox = const DecoratedBox(
+    decoration: BoxDecoration(color: Colors.red),
+  );
+
+  Widget greenBox = const DecoratedBox(
+    decoration: BoxDecoration(color: Colors.green),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Flutter Box Demo")),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: <Widget>[
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                  minWidth: double.infinity, //宽度尽可能大
+                  minHeight: 50.0 //最小高度为50像素
+                  ),
+              child: SizedBox(
+                height: 5.0,
+                child: redBox,
+              ),
+            ),
+            ConstrainedBox(
+              constraints:
+                  const BoxConstraints(minWidth: 60.0, minHeight: 60.0), //父
+              child: ConstrainedBox(
+                constraints:
+                    const BoxConstraints(minWidth: 90.0, minHeight: 20.0), //子
+                child: greenBox,
+              ),
+            )
+          ],
         ),
       ),
     );
